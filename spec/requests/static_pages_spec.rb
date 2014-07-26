387a571d3aac2c04b1cbf708f6fe52d044200f6c
Gabe G'Sell <gabergg@gmail.com>
2014-07-26 14:13:57 -0700

    added full_title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -13,33 +13,38 @@ describe "Static pages" do
 
     it "should have the title 'Home'" do
       visit '/static_pages/home'
-      expect(page).to have_title("#{base_title} | Home")
+      expect(page).to have_title("#{base_title}")
     end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title('| Home')
+    end
+end
+
+describe "Help page" do
+
+  it "should have the content 'Help'" do
+    visit '/static_pages/help'
+    expect(page).to have_content('Help')
   end
 
-  describe "Help page" do
+  it "should have the title 'Help'" do
+    visit '/static_pages/help'
+    expect(page).to have_title("#{base_title} | Help")
+  end
+end
 
-    it "should have the content 'Help'" do
-      visit '/static_pages/help'
-      expect(page).to have_content('Help')
-    end
+describe "About page" do
 
-    it "should have the title 'Help'" do
-      visit '/static_pages/help'
-      expect(page).to have_title("#{base_title} | Help")
-    end
+  it "should have the content 'About Us'" do
+    visit '/static_pages/about'
+    expect(page).to have_content('About Us')
   end
 
-  describe "About page" do
-
-    it "should have the content 'About Us'" do
-      visit '/static_pages/about'
-      expect(page).to have_content('About Us')
-    end
-
-    it "should have the title 'About Us'" do
-      visit '/static_pages/about'
-      expect(page).to have_title("#{base_title} | About Us")
-    end
+  it "should have the title 'About Us'" do
+    visit '/static_pages/about'
+    expect(page).to have_title("#{base_title} | About Us")
   end
+end
 end
