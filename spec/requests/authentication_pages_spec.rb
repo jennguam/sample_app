2f87bed29edc7400a2838205791bb9c1253aeabd
jennguam <j.leung91@gmail.com>
2014-08-07 00:54:37 -0700

    finish user signup

---
+++ spec/requests/authentication_pages_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe "Authentication" do
+
+  subject { page }
+
+  describe "signin page" do
+    before { visit signin_path }
+
+    it { should have_content('Sign in') }
+    it { should have_title('Sign in') }
+  end
+
+  describe "signin" do
+    before { visit signin_path }
+
+  end
+end
